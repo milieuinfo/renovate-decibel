ad06e41ba8dc059676326c71bb4df1ce8964bfc7
cambiph <philippe.cambien@gmail.com>
2020-03-26 13:44:23 +0100

    Initial commit

---
+++ node_modules/vl-ui-pill/dist/index.js
@@ -0,0 +1,5 @@
+module.exports = { 
+    VlPill: require('../dist/vl-pill.src.js').VlPill,
+    VlPillElement: require('../dist/vl-pill.src.js').VlPillElement,
+    VlLabelPill: require('../dist/vl-pill.src.js').VlLabelPill
+}
