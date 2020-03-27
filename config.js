d6639598f385be5f5f854144cef7aee6dba4c71b
cambiph <philippe.cambien@gmail.com>
2020-03-27 14:35:33 +0100

    Update config

--- config.js
+++ config.js
@@ -1,8 +1,13 @@
 module.exports = {
-    "autodiscover": "true",
-    "autodiscoverFilter": "milieuinfo/webcomponent-vl*",
-    "dryRyn": "true",
-    "onboarding": "false"
+    "assignees": ["kevcraey"],
+    "autodiscover": true,
+    "autodiscoverFilter": "milieuinfo/webcomponent-vl-ui*",
+    "labels": ["dependencies", "renovate"],
+    "lockFileMaintenance": { "enabled": true },
+    "masterIssue": true,
+    "masterIssueAutoclose": true,
+    "onboarding": false,
+    "reviewers": ["coemans", "cambiph", "wautergu", "pascalriquier", "SanderKleykens", "Pieter-Beckers"]
 };
 
 
