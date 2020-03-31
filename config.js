ee177a0f9a5d49015b93f9b5c0f12975800ac595
cambiph <philippe.cambien@gmail.com>
2020-03-31 11:42:16 +0200

    Automerge patches en minors Enkel npm packages processen, geen Dockerfiles

--- config.js
+++ config.js
@@ -2,11 +2,19 @@ module.exports = {
     "assignees": ["kevcraey"],
     "autodiscover": true,
     "autodiscoverFilter": "milieuinfo/webcomponent-vl-ui*",
+    "enabledManagers": ["npm"],
     "labels": ["dependencies", "renovate", "chore"],
     "lockFileMaintenance": { "enabled": true },
     "masterIssue": true,
     "masterIssueAutoclose": true,
     "onboarding": false,
+    "packageRules": [
+        {
+            "updateTypes": ["minor", "patch"],
+            "automerge": true,
+            "requiredStatusChecks": null
+        }
+    ],
     "reviewers": ["coemans", "cambiph", "wautergu", "pascalriquier", "SanderKleykens", "Pieter-Beckers"]
 };
 
